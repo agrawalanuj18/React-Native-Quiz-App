bac080ef1c9595161ebf611832816a0db6da9cbf
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-27 19:24:08 +0530

    add Starting Template

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const LotsOfStyles = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.red}>Just Red</Text>
+        <Text style={styles.bigBlue}>Just BigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>BigBlue, then Red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>Red, then BigBlue</Text>
+      </View>
+    );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
+
+export default App;
