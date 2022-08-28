b3a580a32ea6922cb678c37d3102ead7b24d75f5
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-28 20:52:47 +0530

    arrange

--- App.js
+++ App.js
@@ -1,29 +1,4 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const LotsOfStyles = () => {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.red}>Just Red</Text>
-        <Text style={styles.bigBlue}>Just BigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>BigBlue, then Red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>Red, then BigBlue</Text>
-      </View>
-    );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
 export default App;
